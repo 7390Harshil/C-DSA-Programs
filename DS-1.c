a8701201cdbf552cc6d41518e70e3ee7ecadcb1c
7390Harshil <145197531+7390Harshil@users.noreply.github.com>
2023-09-23 11:30:20 +0530

    Add files via upload

---
+++ DS-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,i,
+    a[10];
+    printf("Enter the array size:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
